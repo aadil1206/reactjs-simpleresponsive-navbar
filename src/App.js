1868c76297dc99b9605e6764cc2809c7977a1628
aahiraashrafi <adilmohammadkgn786@gmail.com>
2023-05-18 12:04:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Navbar';
+function App() {
+  return (
+  <Navbar/>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div className='conto'>
+     <div className="l1"><a href="#" className="l12">Site Name</a></div>
+     <div className="list">
+      <ul className="list1">
+        <li className="a1"><a href="#" className="a12">Pricing</a></li>
+        <li className="a1"><a href="#" className="a12">Features</a></li>
+        <li className="a1"><a href="#" className="a12">About</a></li>
+        <li className="a1"><a href="#" className="a12">Contact</a></li>
+      </ul>
+     </div>
+
+    </div>
+
+  )
+}
+
+export default Navbar
